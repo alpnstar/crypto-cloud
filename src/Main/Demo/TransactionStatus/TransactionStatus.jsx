bc16270391f54e6a16e2d838b2753271f027230f
Satiyadzhiev Daud Adjakavovich <81386387+alpnstar@users.noreply.github.com>
2023-06-08 19:09:12 +0300

    commit

--- src/Main/Demo/TransactionStatus/TransactionStatus.jsx
+++ src/Main/Demo/TransactionStatus/TransactionStatus.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 export default function TransactionStatus(props) {
     return (
         <div className="demo__transactionStatus">
-            <img src="images/loader_accent.ede5cf0.svg" alt="" />
+            <img src="./imgs/loader_accent.ede5cf0.svg" alt="" />
             <div className="demo__transactionStatus-content">
                 <h2>Ищем транзакцию в сети Blockchain</h2>
                 <span className="demo__transactionStatus-note">
