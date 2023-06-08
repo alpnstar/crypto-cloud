0ec921435915ff33665d2155d6d2b6dbc6d21e4e
Satiyadzhiev Daud Adjakavovich <81386387+alpnstar@users.noreply.github.com>
2023-06-08 18:39:06 +0300

    commit

--- src/Main/Demo/TransactionStatus/TransactionStatus.jsx
+++ src/Main/Demo/TransactionStatus/TransactionStatus.jsx
@@ -3,10 +3,13 @@ import React, { useEffect, useState } from 'react';
 export default function TransactionStatus(props) {
     return (
         <div className="demo__transactionStatus">
-            <h2>Ищем транзакцию в сети Blockchain</h2>
-            <span className="demo__transactionStatus-note">
-                Обычно транзакции выходят в сеть в течение 15 минут…
-            </span>
+            <img src="images/loader_accent.ede5cf0.svg" alt="" />
+            <div className="demo__transactionStatus-content">
+                <h2>Ищем транзакцию в сети Blockchain</h2>
+                <span className="demo__transactionStatus-note">
+                    Обычно транзакции выходят в сеть в течение 15 минут…
+                </span>
+            </div>
         </div>
     )
 }
